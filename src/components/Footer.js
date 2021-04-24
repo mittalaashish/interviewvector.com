1a6e853c936017aa9e33d0616615df86422c55d4
Shivam Vats <shivamvats0987@gmail.com>
2021-04-24 23:48:52 +0530

    refactoring of code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,72 +1,71 @@
-import React from 'react'
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-   
-  }));
-  
+  root: {
+    flexGrow: 1,
+  },
+}));
+
 const Footer = () => {
-    const classes = useStyles();
-    return (
-        // <div className="section footer-color">
-        <div className="footer-color">
-        <Container>
-       
-        <Grid container   >
-      
-      
-               
-               <Grid item xs={6} sm={6} >
-              {/* <div className="" style={{ marginTop:"-30px" }}> */}
-        
-              <span className="footer-reach">Reach Us</span>
-              </Grid>
-              <Grid item  xs={6} sm={6} >
-              {/* <div className="" style={{ marginTop:"-30px" }}> */}
-        
-              <div style={{display:'flex',justifyContent:"flex-end"}}>
-                <div >
-                <a href="https://www.linkedin.com/company/interviewvector/" target="_blank" rel="noopener noreferrer">  <img src="tweet.png"></img></a>
-                </div>
-                <div className="ml-15">
-                
-                <a href="https://angel.co/company/interviewvector" target="_blank" rel="noopener noreferrer">
-                  <img src="link.png"></img></a>
-                </div>
-            </div>
-              </Grid>
-            
-              <Grid item  xs={12} sm={6} className="footer_contact" container>
-              
-                  <div>
-              <p className="footer-email" >contact@interviewvector.com</p>
-              </div>
-              <div className="footer-line" style={{marginLeft:"5px",marginRight:"5px"}}>|</div>
+  const classes = useStyles();
+  return (
+    <div className="footer-color">
+      <Container>
+        <Grid container>
+          <Grid item xs={6} sm={6}>
+            <span className="footer-reach">Reach Us</span>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
               <div>
-              <p className="footer-email">
-                +91-97068-70735
-              </p>
+                <a
+                  href="https://www.linkedin.com/company/interviewvector/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  <img src="tweet.png"></img>
+                </a>
               </div>
-             
-              </Grid>
-              <Grid item  xs={12} sm={6} className="copyright">
-              
-              <div className="footer-email">Copyright © 2021</div>
-              
-              <div className="footer-email" style={{marginLeft:"5px"}}>interviewvector</div>
+              <div className="ml-15">
+                <a
+                  href="https://angel.co/company/interviewvector"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="link.png"></img>
+                </a>
+              </div>
+            </div>
+          </Grid>
 
-             
-              </Grid>
-            
-      </Grid>
-      
+          <Grid item xs={12} sm={6} className="footer_contact" container>
+            <div>
+              <p className="footer-email">contact@interviewvector.com</p>
+            </div>
+            <div
+              className="footer-line"
+              style={{ marginLeft: "5px", marginRight: "5px" }}
+            >
+              |
+            </div>
+            <div>
+              <p className="footer-email">+91-97068-70735</p>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} className="copyright">
+            <div className="footer-email">Copyright © 2021</div>
+
+            <div className="footer-email" style={{ marginLeft: "5px" }}>
+              interviewvector
+            </div>
+          </Grid>
+        </Grid>
       </Container>
-      </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Stand.js
+++ src/components/Stand.js
@@ -1,37 +1,38 @@
-import React from 'react'
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import AboutUs from './AboutUs';
-
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import AboutUs from "./AboutUs";
 
 const Stand = () => {
-    return (
-        <div className="stand_parent">
-        <Container>
-            <Grid container  sm={12} xm={12}>
-            
-        
-          {/* <h2 className="heading-9">WHERE DO WE STAND RIGHT NOW?</h2> */}
-          <Grid item xs={12} sm={12} >
-              <h3 className="heading-9" >WHERE DO WE STAND RIGHT NOW?</h3>
-              
-          <div className="div-block-2 mock-up mock2 mock3" />
+  return (
+    <div className="stand_parent">
+      <Container>
+        <Grid container sm={12} xm={12}>
+          <Grid item xs={12} sm={12}>
+            <h3 className="heading-9">WHERE DO WE STAND RIGHT NOW?</h3>
+
+            <div className="div-block-2 mock-up mock2 mock3" />
           </Grid>
-          
-          <Grid container  xs={12} className="stand_card_parent">
-          <Grid item container sm={12} md={4} className="stand_card stand_right_line" >
-           
-                <div>
-              <img
-                src="https://uploads-ssl.webflow.com/60733d2745729b18912a257d/6073866235c4a2082a7b2d7e_icon%202.svg"
-                loading="lazy"
-                alt=""
-                className="image-10"
-              />
+
+          <Grid container xs={12} className="stand_card_parent">
+            <Grid
+              item
+              container
+              sm={12}
+              md={4}
+              className="stand_card stand_right_line"
+            >
+              <div>
+                <img
+                  src="https://uploads-ssl.webflow.com/60733d2745729b18912a257d/6073866235c4a2082a7b2d7e_icon%202.svg"
+                  loading="lazy"
+                  alt=""
+                  className="image-10"
+                />
               </div>
               <div>
-              <h1 className="heading-11">6000 +</h1>
+                <h1 className="heading-11">6000 +</h1>
               </div>
               <div className="text-block-2 text-center">
                 developers hours
@@ -39,7 +40,13 @@ const Stand = () => {
                 saved
               </div>
             </Grid>
-            <Grid container item sm={12} md={4}  className="stand_card stand_right_line">
+            <Grid
+              container
+              item
+              sm={12}
+              md={4}
+              className="stand_card stand_right_line"
+            >
               <img
                 src="https://uploads-ssl.webflow.com/60733d2745729b18912a257d/6073866272cc435cd18928da_icon%203.svg"
                 loading="lazy"
@@ -51,11 +58,10 @@ const Stand = () => {
               <div className="text-block-2 text-center">
                 hours to interview
                 <br />
-                from  scheduling
-               
+                from scheduling
               </div>
-              </Grid>
-              <Grid item container sm={12} md={4} className="stand_card" >
+            </Grid>
+            <Grid item container sm={12} md={4} className="stand_card">
               <img
                 src="https://uploads-ssl.webflow.com/60733d2745729b18912a257d/60738662f38e75e43fbabf06_icon%204.svg"
                 loading="lazy"
@@ -68,17 +74,13 @@ const Stand = () => {
                 hours after
                 <br />
                 interview
-               
               </div>
-           </Grid>
-         
-      
-     </Grid>
             </Grid>
-        </Container>
-        </div>
-
-    )
-}
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
 export default Stand;
